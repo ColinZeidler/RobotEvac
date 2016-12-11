33222a043ea84a6f3605a514eea284646bfc7388
Colin Zeidler <c.zeidler.wc@gmail.com>
2016-12-10 20:11:45 -0500

    added min and max times, output times to file named after mode run

--- main.py
+++ main.py
@@ -86,6 +86,11 @@ while True:
         elif run_count >= 50:
             running = False
             average = sum(times)/len(times)
+            with open("timings-{}.txt".format(mode)) as f:
+                for time in times:
+                    f.write("{}\n".format(time))
+                f.write("max time: {}\nmin time: {}\n average time: {}".format(max(times), min(times), average))
+            print "max time: {}\nmin time: {}".format(max(times), min(times))
             print "average time: {}".format(average)
 
     pygame.display.flip()
